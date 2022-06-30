517bea7e5706892e73ca95c810dfe7c15b6ce7c5
Nur <drmzay12@gmail.com>
2022-06-30 09:03:09 +0300

    Sidebar edited.

---
+++ src/app/sidebar/nav-data.ts
@@ -0,0 +1,22 @@
+export const navbarData = [
+    {   routeLink: 'baslik',
+        icon: 'pi-align-justify',
+        label: 'Başlık'
+    },
+    {   routeLink: 'form',
+        icon: 'pi-server',
+        label: 'Form'
+    },
+    {   routeLink: 'belge',
+        icon: 'pi-file-o',
+        label: 'Belge'
+    },
+    {   routeLink: 'baglanti',
+        icon: 'pi-external-link',
+        label: 'Bağlantı'
+    },
+    {   routeLink: 'checkbox',
+        icon: 'pi-check-square',
+        label: 'Check Box'
+    }
+];
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit, Output, EventEmitter, HostListener} from '@angular/core';
+import {navbarData} from './nav-data';
+
+
+interface SidenavToggle {
+    screenWidth: number;
+    collapsed: boolean;
+}
+
+@Component({
+    selector: 'app-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.scss']
+})
+
+export class SidebarComponent implements OnInit {
+
+    @Output() onToggleSidenav: EventEmitter<SidenavToggle> = new EventEmitter();
+    collapsed = false;
+    screenWidth = 0;
+    navData = navbarData;
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event: any) {
+        this.screenWidth = window.innerWidth;
+        if (this.screenWidth <= 768) {
+            this.collapsed = false;
+            this.onToggleSidenav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+        }
+    }
+
+    closeSidenav(): void {
+        this.collapsed = false;
+        this.onToggleSidenav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+    }
+
+    toggleCollopsed(): void {
+        this.collapsed = !this.collapsed;
+        this.onToggleSidenav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+    }
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        this.screenWidth = window.innerWidth;
+    }
+}
