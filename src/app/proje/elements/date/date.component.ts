76ef9683468ca324d215788462ed5834dabafbfb
lKhanl <oguzhanercelik@eskisehir.edu.tr>
2022-07-01 16:52:52 +0300

    splash screen

---
+++ src/app/proje/elements/date/date.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-date',
+    templateUrl: './date.component.html',
+    styleUrls: ['./date.component.scss']
+})
+export class DateComponent implements OnInit {
+
+    hide: boolean;
+    passwordInput: string;
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
