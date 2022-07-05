4eea08ffd458be85d5bdc52f81db15b7292899bc
OzzkanBulut <ozkanb66@gmail.com>
2022-07-05 10:13:19 +0300

    Divider Updated

--- src/app/proje/sidebarelements/divider/divider.component.ts
+++ src/app/proje/sidebarelements/divider/divider.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./divider.component.scss']
 })
 export class DividerComponent implements OnInit {
-
+    val: number;
+    height: number;
+    below: number;
+    above: number;
+    space: number;
   constructor() { }
 
   ngOnInit(): void {
