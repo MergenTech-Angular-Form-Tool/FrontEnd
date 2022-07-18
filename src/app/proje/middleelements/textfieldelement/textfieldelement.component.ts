21667bcfa42be064ba15585c9ea580336668caab
OzzkanBulut <ozkanb66@gmail.com>
2022-07-18 17:52:08 +0300

    Save button for textfield element

--- src/app/proje/middleelements/textfieldelement/textfieldelement.component.ts
+++ src/app/proje/middleelements/textfieldelement/textfieldelement.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {SharedDataService} from '../../../demo/service/sharedataservice';
 import {CollapsedRightBarService} from '../../../demo/service/collapsedRightBarService';
+import {SaveService} from '../../../demo/service/saveservice';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-textfieldelement',
@@ -8,21 +10,39 @@ import {CollapsedRightBarService} from '../../../demo/service/collapsedRightBarS
     styleUrls: ['./textfieldelement.component.scss']
 })
 export class TextfieldelementComponent implements OnInit {
+    clickEventsubscription: Subscription;
+    message: string;
+    placeholder: string;
+    header: string;
+    smalltext: string;
+    change: boolean;
 
-    constructor(private shared: SharedDataService, private collapsed: CollapsedRightBarService) {
+    constructor(private sharedDataService: SharedDataService, private collapsedRightBarService: CollapsedRightBarService,
+                private save: SaveService) {
+
+        this.clickEventsubscription = this.save.getClickEvent().subscribe(() => {
+            this.pass();
+        });
     }
 
     ngOnInit(): void {
+        this.save.currentMessage.subscribe( message => this.message = message);
     }
 
     delete($event: any) {
         $event.currentTarget.parentElement.parentElement.parentElement.parentElement.remove();
-        this.collapsed.close();
-        this.shared.changeMessage('');
     }
 
     edit($event: any) {
-        this.shared.changeMessage($event.currentTarget.parentElement.parentElement.parentElement.parentElement.id);
-        this.collapsed.open();
+        this.sharedDataService.changeMessage($event.currentTarget.parentElement.parentElement.parentElement.parentElement.id);
+        // this.collapsedRightBarService.open();
     }
+    pass(){
+        this.header = this.message;
+
+
+    }
+
+
+
 }
--- src/app/proje/sidebarelements/textfield/textfield.component.ts
+++ src/app/proje/sidebarelements/textfield/textfield.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SaveService} from '../../../demo/service/saveservice';
+import {SharedDataService} from '../../../demo/service/sharedataservice';
 
 @Component({
     selector: 'app-textfield',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./textfield.component.scss']
 })
 export class TextfieldComponent implements OnInit {
+    constructor(private save: SaveService, private share: SharedDataService) { }
     value1: any;
-    constructor() { }
+    question: string;
+    placeholder: string;
+    subtext: string;
+    message: string;
+    id: string;
 
     ngOnInit(): void {
+        this.save.currentMessage.subscribe(message => this.message = message);
+        this.share.currentMessage.subscribe(id => this.id = id );
     }
+
+    onSubmit() {
+        this.save.changeMessage(this.question);
+        const element = document.getElementById(this.id);
+        // console.log(element.firstChild.firstChild.nextSibling.nextSibling.firstChild.firstChild.);
+        // tslint:disable-next-line:max-line-length
+        element.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild.firstElementChild.innerHTML = this.message;
+
+        this.save.sendClickEvent();
+        // console.log(this.question, this.subtext, this.placeholder);
+
+
+
+    }
+
+
+
 
 }
